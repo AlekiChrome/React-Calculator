19849f596865d7e8221b87447918fd88619672c8
Candice Agard <candiceagard@Candices-MBP.home>
2021-05-05 11:45:12 -0400

    buttons rendered and styled

--- src/App.js
+++ src/App.js
@@ -1,11 +1,40 @@
-
 import './App.css';
+import Buttons from "./components/Buttons";
+
 
 const App = () => {
   return (
     <div className="App">
-      <div className="calcMain">
-    
+      <div className="calc-main">
+        
+        <div className="button-row">
+          <Buttons symbol="7"/>
+          <Buttons symbol="8"/>
+          <Buttons symbol="9"/>
+          <Buttons symbol="/"/>
+        </div>
+
+        <div className="button-row">
+          <Buttons symbol="4"/>
+          <Buttons symbol="5"/>
+          <Buttons symbol="6"/>
+          <Buttons symbol="*"/>
+        </div>
+
+        <div className="button-row">
+          <Buttons symbol="1"/>
+          <Buttons symbol="2"/>
+          <Buttons symbol="3"/>
+          <Buttons symbol="+"/>
+        </div>
+
+        <div className="button-row">
+          <Buttons symbol="0"/>
+          <Buttons symbol="."/>
+          <Buttons symbol="="/>
+          <Buttons symbol="-"/>
+        </div>
+
       </div>
     </div>
   );
---
+++ src/components/Buttons.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import "./Buttons.css"
+
+const Buttons = ({ symbol }) => {
+    return (
+        <div className="button-main">
+            {symbol}
+        </div>
+    )
+}
+
+export default Buttons
